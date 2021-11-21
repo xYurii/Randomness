8e348fca8e97bcf30c1a9f40ea81508dbd90f582
Yuri <92718516+xYurii@users.noreply.github.com>
2021-11-21 03:34:57 -0300

    Update utils.js

--- functions/utils.js
+++ functions/utils.js
@@ -3,11 +3,6 @@ const censorName = require('./plugins/censorName.js')
 const convertAbbrev = require('./plugins/convertAbbrev.js')
 const abbrev = require('./plugins/abbrev.js')
 const getTime = require('./plugins/getTime.js')
-const findUser = require('./plugins/findUser.js')
-const calcTaxa = require('./plugins/calcTaxa.js')
-const checkBan = require('./plugins/checkBan.js')
-const manageBl = require('./plugins/managerBl.js')
-const manageMoney = require('./plugins/manageMoney.js')
 
 module.exports = {
     rand: rand,
@@ -15,9 +10,4 @@ module.exports = {
     notAbbrev: convertAbbrev,
     abbrev: abbrev,
     getTime: getTime,
-    findUser: findUser,
-    calcTaxa: calcTaxa,
-    checkBan: checkBan,
-    manageBl: manageBl,
-    manageSouls: manageMoney
 }
